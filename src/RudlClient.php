9513bbe207713ebb4f58060b81120ed9cb20dca3
matthes <leuffen@continue.de>
2017-08-08 19:58:27 +0200

    Fix for ru_usage with keepalive apache

--- src/RudlClient.php
+++ src/RudlClient.php
@@ -24,6 +24,8 @@ class RudlClient
 
     private $mStartTime = null;
 
+    private $mAnonymizer = [];
+
     public function __construct($logIp, $logPort=62111)
     {
         $this->mStartupRu = getrusage(); // Collect the startup RU-Usage (To prevent double-counting when apache keepalive is switched on)
@@ -44,12 +46,21 @@ class RudlClient
     }
 
 
+    public function addAnonymizer ($preg, $replace) {
+        $this->mAnonymizer[] = [$preg, $replace];
+    }
+
 
 
 
     public function registerResourceLogging () {
+
         register_shutdown_function(function () {
             $ru = getrusage();
+            $url = "//" . @$_SERVER["HTTP_HOST"] . @$_SERVER["REQUEST_URI"];
+            foreach ($this->mAnonymizer as $cur) {
+                $url = preg_replace($cur[0], $cur[1], $url);
+            }
             $rr = [
                 $this->mSysId,
                 gethostname(),
@@ -58,7 +69,7 @@ class RudlClient
                 memory_get_peak_usage(),
                 ($ru["ru_utime.tv_sec"] + ($ru["ru_utime.tv_usec"] * 0.000001)) - ($this->mStartupRu["ru_utime.tv_sec"] + ($this->mStartupRu["ru_utime.tv_usec"] * 0.000001)) + 0.001,
                 ($ru["ru_stime.tv_sec"] + ($ru["ru_stime.tv_usec"] * 0.000001)) - ($this->mStartupRu["ru_stime.tv_sec"] + ($this->mStartupRu["ru_stime.tv_usec"] * 0.000001)) + 0.001,
-                "//" . @$_SERVER["HTTP_HOST"] . @$_SERVER["REQUEST_URI"],
+                $url,
                 (microtime(true) - $this->mStartTime)
             ];
             $msg = "G11:" . json_encode($rr);
